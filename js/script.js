81884127663e037b7d1de6a73441f3544fc09598
Ashwin Patti <ashwin.patti@gmail.com>
2013-12-14 21:55:17 -0800

    UI

--- js/script.js
+++ js/script.js
@@ -1,40 +1,51 @@
+
+Parse.initialize("JQoRy7meJUr3GboQNHkC4HcVzVHaG4GPFnhLmC2z", "ALjfHifDxF8YNuCY3wGF8f0d2Geg1nSLE7VqD3Be");
 // Define a new module. 
-var app = angular.module("TellAStoryApp", []);
+var app = angular.module("TellAStoryApp",[]);
 
-// Create and register the new "instagram" service
-app.factory('instagram', function($resource){
-
-	return {
-		fetchPopular: function(callback){
-
-			// The ngResource module gives us the $resource service. It makes working with
-			// AJAX easy. Here I am using a client_id of a test app. Replace it with yours.
-
-			var api = $resource('https://api.instagram.com/v1/media/popular?client_id=:client_id&callback=JSON_CALLBACK',{
-				client_id: '642176ece1e7445e99244cec26f4de1f'
-			},{
-				// This creates an action which we've chosen to name "fetch". It issues
-				// an JSONP request to the URL of the resource. JSONP requires that the
-				// callback=JSON_CALLBACK part is added to the URL.
-
-				fetch:{method:'JSONP'}
-			});
-
-			api.fetch(function(response){
-
-				// Call the supplied callback function
-				callback(response.data);
-
-			});
-		}
-	}
-
-});
 
 // The controller. Notice that I've included our instagram service which we
 // defined below. It will be available inside the function automatically.
 
-app.controller('StoryController',function($scope){
+app.controller('StoryController',function($scope,$http){
+    var StoryLine = Parse.Object.extend("STORY_LINE");
+    var query = new Parse.Query(StoryLine);
+    query.find({
+      success: function(StoryLines) {
+		/*for(var i=0;i<StoryLines.length;i++)
+		{
+			var fid = StoryLine.get("fid");
+			var title = StoryLine.get("title");
+			var objectId = StoryLine.id;
+			var updatedAt = StoryLine.updatedAt;
+			var createdAt = StoryLine.createdAt;
+			console.log("Fid: " + fid);
+			console.log("title: " + title);
+			console.log("objectId: " + objectId);
+       }*/
+	   var FrameObject = Parse.Object.extend("FRAME");
+	   var queryFrames = new Parse.Query(FrameObject);
+	   queryFrames.equalTo("sid",1);
+	   queryFrames.find({
+		success:function(Frames)
+		{
+			for(var i=0;i<Frames.length;i++)
+			{
+				//JSON
+			}
+		},
+		error: function(error){
+			alert("Error: "+error.code+" " + error.message);
+		}
+	   });
+        // The object was retrieved successfully.
+      },
+      error: function(object, error) {
+        // The object was not retrieved successfully.
+        // error is a Parse.Error with an error code and description.
+      }
+    });
+	
 	$scope.stories=[
 		{'story':'Story 1',
 		 'pic':'images/icon_01.png'},
@@ -50,9 +61,6 @@ app.controller('StoryController',function($scope){
 		 'pic':'images/icon_02.png'},
 		 {'story':'Story 7',
 		 'pic':'images/icon_03.png'},
-		 {'story':'Story 8',
-		 'pic':'images/icon_04.png'},
-		 {'story':'Story 9',
-		 'pic':'images/icon_01.png'},
+		 
 	];
 });
